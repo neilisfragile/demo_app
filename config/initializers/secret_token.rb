7b4047b59793f542240ada27f665675e690533f6
Neil Johnson <neil@fragile.org.uk>
2013-10-16 21:25:32 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '3f74c77428af4e6939bd4edbe2146879e444700dbc52004981a0d2d33fb09c5509316e923fe333176e41c73161891432e0a1545587bea3d13a438d51aa59e022'
